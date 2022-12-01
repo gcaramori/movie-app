f0a60a84b9861f0db964d3474506100ca12a5a3a
Guilherme <guilherme@sunsetsolutions.com.br>
2022-12-01 11:32:49 -0300

    Deploying to vercel

--- server/app/routes/router.js
+++ server/app/routes/router.js
@@ -6,30 +6,33 @@ const reviewController = require('../controllers/reviewController');
 const auth = require("../middleware/auth");
 
 route.get('/*', (req, res) => {
-    res.status(200).send('OLÁ MUNDO!');
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
 
 //API
 
 //users
-route.post('/api/users', userController.create);
-route.post('/api/users/find', userController.find);
-route.put('/api/users', userController.update);
-route.delete('/api/users', userController.delete);
-route.post('/api/signin', userController.signin);
-route.get('/api/logout', userController.logout);
+route.post('/users', userController.create);
+route.post('/users/find', userController.find);
+route.put('/users', userController.update);
+route.delete('/users', userController.delete);
+route.post('/signin', userController.signin);
+route.get('/logout', userController.logout);
 
 //watch_list
-route.post('/api/watch_list', watchListController.add);
-route.post('/api/watch_list/find', watchListController.find);
-route.put('/api/watch_list', auth, watchListController.updateStatus);
-route.delete('/api/watch_list', watchListController.remove);
+route.post('/watch_list', watchListController.add);
+route.post('/watch_list/find', watchListController.find);
+route.put('/watch_list', auth, watchListController.updateStatus);
+route.delete('/watch_list', watchListController.remove);
 
 
 //review
-route.post('/api/reviews', reviewController.add);
-route.post('/api/reviews/find', reviewController.find);
-route.put('/api/reviews', auth, reviewController.update);
-route.delete('/api/reviews', reviewController.remove);
+route.post('/reviews', reviewController.add);
+route.post('/reviews/find', reviewController.find);
+route.put('/reviews', auth, reviewController.update);
+route.delete('/reviews', reviewController.remove);
 
 module.exports = route;

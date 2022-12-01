a515f1e5d1b68d5f6417d939fecae224bdae3e68
Guilherme <guilherme@sunsetsolutions.com.br>
2022-12-01 12:53:38 -0300

    Deploying to vercel

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '../client', 'build')));
+app.use(express.static(path.join(__dirname, '../client', '/build')));
 
 app.listen(PORT, () => {
   console.log(`Server is on ${PORT}!`); 
